7a78be2e5f004936417a1d527b5a5b2a3b63be7c
shivakar <shivakar@ch.iitr.ac.in>
2021-11-09 23:18:58 +0530

    [shivakar] ADD : UC1 is added

---
+++ BinarySearchTree.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearch
+{
+    class BinarySearchTree<T> where T: IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BinarySearchTree<T> LeftTree { get; set; }
+        public BinarySearchTree<T> RightTree { get; set; }
+
+        public BinarySearchTree(T nodeData)
+        {
+            NodeData = nodeData;
+            LeftTree = null;
+            RightTree = null;   
+        }
+
+        int leftCount = 0, rightCount = 0;
+       
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new BinarySearchTree<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new BinarySearchTree<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+
+        public void Display()
+        {
+            if(this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if(this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+                    
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+
+
+
+        }
+    }
+}
